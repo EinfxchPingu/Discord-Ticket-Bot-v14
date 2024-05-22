9f464f6e4c992d376127548ead17ab925f538ddf
EinfxchPingu <118051755+EinfxchPingu@users.noreply.github.com>
2024-05-22 15:45:58 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,54 +1,67 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Permissions, EmbedBuilder, StringSelectMenuBuilder, PermissionFlagsBits, StringSelectMenuOptionBuilder, ActionRowBuilder } = require('discord.js');
+const { Permissions, PermissionFlagsBits, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ticket')
-    .setDescription('📚〢Ticket Commands')
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .setDMPermission(false)
+    .setDescription('📚〢Ticket Commands')
     .addSubcommand(subcommand =>
       subcommand
         .setName('setup')
-        .setDescription('🔧〢Create a Ticket'),
-  ),
+        .setDescription('🔧〢Erstelle ein Ticketsystem'))
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   async execute(interaction) {
-    
-    if (interaction.options.getSubcommand() === 'setup') {
+    if (!interaction.guild) {
+      return interaction.reply({ content: '`❌`〢Dieser Befehl kann nur in einem Server ausgeführt werden.', ephemeral: true });
+    }
       
+      if (interaction.options.getSubcommand() === 'setup') {
       const ticketEmbed = new EmbedBuilder()
         .setColor(0x0099ff)
-        .setDescription(`### \`👋\`〢Create a Ticket
-・ Welcome to our Ticket Support, the first point of contact if you have a problem or just want to contact the server team!
-### \`📋\`〢Support Selection
-・ Select what kind of support you need in the select menu below!
-
-x General Support
-x Report a User or Bug
-x Join the team
+        .setDescription(`### \`🎫\` | TICKET SUPPORT
+> \`👤\` | **Anmerkung zum Ticket Support**
+> Wir bitte dich nur Ticket's zu öffnen die mit deinen ausgewählten Thema übereinstimmen! 
+ 
+> \`📋\` | **Diese auswahlen gibt es:** 
+> | Allgemeiner Support
+> | Report
+> | Teamler Bewerbung 
+> | Geld- Itemausleih 
+ 
+> \`📰\` | **Information zum Ticket Support:**
+> Um ein Ticket zu öffnen drücke unten auf das Select Menü und wähle eine Kategorie aus. Es wird sich möglicher weiße ein Fenster eröffnen, wo du dein Anliegen genauer beschreiben musst!
 `);
         const ticketselect = new StringSelectMenuBuilder()
 			.setCustomId('ticketselect')
-			.setPlaceholder('📌〢Select a Option...')
+			.setPlaceholder('📌〢Wähle eine Option...')
 			.addOptions(
 				new StringSelectMenuOptionBuilder()
-					.setLabel('📌 x General Support')
-					.setDescription('Click on me to open a ticket')
+					.setLabel('📌 x Allgemeiner Support')
+					.setDescription('Klicken mich an um ein Ticket zu öffnen')
 					.setValue('support'),
 				new StringSelectMenuOptionBuilder()
-					.setLabel('📡 x Report a User or Bug')
-					.setDescription('Click on me to open a ticket')
-					.setValue('report'),
+					.setLabel('📡 x Einen Benutzer melden')
+					.setDescription('Klicken mich an um ein Ticket zu öffnen')
+					.setValue('meldung'),
 				new StringSelectMenuOptionBuilder()
-					.setLabel('🚀 x Join the team')
-					.setDescription('Click on me to open a ticket')
-					.setValue('apply'),
-                
+					.setLabel('🚀 x Bewerbung')
+					.setDescription('Klicken mich an um ein Ticket zu öffnen')
+					.setValue('bewerbung'),
+				new StringSelectMenuOptionBuilder()
+					.setLabel('📨 x Geld- Itemausleih')
+					.setDescription('Klicken mich an um ein Ticket zu öffnen')
+					.setValue('ausleih'),
+                new StringSelectMenuOptionBuilder()
+					.setLabel('❌ x Interaction Abbrechen')
+					.setDescription('Klicken mich an um die Interaction abzubrechen')
+					.setValue('cancel'),
+			
 			);
       const row = new ActionRowBuilder()
 			.addComponents(ticketselect);
       await interaction.channel.send({ embeds: [ticketEmbed], components: [row] });
-      await interaction.reply({ content: '`✅`〢The ticket system has been successfully created!', ephemeral: true });
+      await interaction.reply({ content: '`✅`〢Das Ticketsystem wurde erfolgreich erstellt!', ephemeral: true });
     }
   },
 };
